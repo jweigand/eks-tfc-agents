00375901aefbf6eb7352cd02965482b7a29204c8
John Weigand <john.weigand@hashicorp.com>
2023-11-28 13:13:55 -0500

    stand up agent

---
+++ eks/irsa.tf
@@ -0,0 +1,19 @@
+module "iam_eks_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "tfc-agent"
+
+  role_policy_arns = {
+    policy = data.aws_iam_policy.admin.arn
+  }
+
+  oidc_providers = {
+    eks = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["${module.tfc-cloud-agent.namespace}:${module.tfc-cloud-agent.service_account_name}"]
+    }
+  }
+}
+
+data "aws_iam_policy" "admin" {
+  name        = "AdministratorAccess"
+}
---
+++ eks/tfc_agent.tf
@@ -0,0 +1,13 @@
+module "tfc-cloud-agent" {
+  source = "github.com/jweigand/terraform-kubernetes-tfc-cloud-agent"
+
+    tfc_agent_token = data.aws_secretsmanager_secret_version.tfc_agent_token_eks.secret_string
+}
+
+data "aws_secretsmanager_secret" "tfc_agent_token_eks" {
+  name = "tfc_agent_token_eks"
+}
+
+data "aws_secretsmanager_secret_version" "tfc_agent_token_eks" {
+  secret_id     = data.aws_secretsmanager_secret.tfc_agent_token_eks.id
+}
