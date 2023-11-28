4900b44b2a92b3de4b632a2b9da258efb20aa5d8
John Weigand <john.weigand@hashicorp.com>
2023-11-28 14:00:07 -0500

    aws provider

---
+++ agent_test/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0"
+
+  cloud {
+    organization = "weigand-hcp"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "eks-tfc-agent-irsa"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
